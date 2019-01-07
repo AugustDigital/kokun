f104a03a04c4b817c41da88f4bd5ec46aeaf78a6
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2019-01-07 13:00:13 -0500

    titan config added

--- titanrc.js
+++ titanrc.js
@@ -1,6 +1,26 @@
 module.exports = {
-    "host": "http://127.0.0.1",
-    "port": 8545,
-    "defaultAccount": "",
-    "password": "PLAT4life"
-}
+    defaultBlockchain: "aion",
+    blockchains: {
+      aion: {
+        networks: {
+          development: {
+            host: "http://178.128.227.209:8545",
+            defaultAccount: "",
+            password: ""
+          },
+          mainnet: {
+            host:
+              "http://api.nodesmith.io/v1/aion/mainnet/jsonrpc?apiKey=451ea61711c4409aaa12fb9394d008b8",
+            defaultAccount: "",
+            password: ""
+          },
+          testnet: {
+            host:
+              "https://api.nodesmith.io/v1/aion/testnet/jsonrpc?apiKey=451ea61711c4409aaa12fb9394d008b8",
+            defaultAccount: "",
+            password: ""
+          }
+        }
+      }
+    }
+  };
